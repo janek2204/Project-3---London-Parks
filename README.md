a25b629005963408f36d6d211f8b2f8c14e420cc
piotr <piotr.jankowski2204@gmail.com>
2021-12-15 15:17:11 +0000

    copying project to deploy

---
+++ README.md
@@ -0,0 +1,7 @@
+# SEI-Project-3-London-Parks
+
+Team Members: 
+Matthew 
+Mariana
+Piotr 
+let se if that works
---
+++ config/environment.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const port = 4000
+// export const dbURI = 'mongodb://127.0.0.1:27017/London'
+export const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/london-parks-api'
+// export const dbURI = 'mongodb://localhost/london-parks-api'
+export const secret = 'dsh823FF2asd$d'
