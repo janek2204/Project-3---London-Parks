a07506472891a5aa10685c84bd0ea2c1c0e4f475
piotr <piotr.jankowski2204@gmail.com>
2023-10-23 19:19:56 +0100

    reinstaling outdated packages, changing component style

--- front-end/src/components/ParkPage.js
+++ front-end/src/components/ParkPage.js
@@ -1,45 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-import { Header, Divider, Grid, Segment, Container, GridColumn } from 'semantic-ui-react'
-import Favourite from './Favourite.js'
-import Weather from './Weather.js'
-import ReactMapGl from 'react-map-gl'
-import Carousel from "react-responsive-carousel/lib/js/components/Carousel/index"
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import Planner  from './Planner.js'
-import ParkInfo from './ParkInfo'
-import Activities from './Activities'
-import { UserComment } from './UserComment'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import {
+  Header,
+  Divider,
+  Grid,
+  Segment,
+  Container,
+  GridColumn,
+} from "semantic-ui-react";
+import Favourite from "./Favourite.js";
+import Weather from "./Weather.js";
+import Map from "react-map-gl";
+import Carousel from "react-responsive-carousel/lib/js/components/Carousel/index";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Planner from "./Planner.js";
+import ParkInfo from "./ParkInfo";
+import Activities from "./Activities";
+import { UserComment } from "./UserComment";
 
 const ParkPage = () => {
-
-  const { id } = useParams()
-  const [park, setPark] = useState(null)
-  const [lat, setLat] = useState()
-  const [long, setLong] = useState()
+  const { id } = useParams();
+  const [park, setPark] = useState(null);
+  const [lat, setLat] = useState();
+  const [long, setLong] = useState();
 
   useEffect(() => {
     const getData = async () => {
-      const { data: park } = await axios.get(`/api/london-parks-api/${id}`)
-      setPark(park)
-      setLat(parseFloat(park.latitude))
-      setLong(parseFloat(park.longitude))
-    }
-    getData()
-  }, [id])
+      const { data: park } = await axios.get(`/api/london-parks-api/${id}`);
+      setPark(park);
+      setLat(parseFloat(park.latitude));
+      setLong(parseFloat(park.longitude));
+    };
+    getData();
+  }, [id]);
 
-  
   return (
     <>
-      {park &&
-      <><Segment raised className='regionTitle'>
-        <Header as={'h1'} textAlign={'center'} id='parkHeader' color='green'>
-          {park.title} 🍁
-          </Header>
+      {park && (
+        <>
+          <Segment raised className="regionTitle">
+            <Header
+              as={"h1"}
+              textAlign={"center"}
+              id="parkHeader"
+              color="green"
+            >
+              {park.title} 🍁
+            </Header>
           </Segment>
-        <Container>
-        <Carousel
+          <Container>
+            <Carousel
               showThumbs={true}
               infiniteLoop={true}
               autoPlay={true}
@@ -48,15 +59,20 @@ const ParkPage = () => {
               autoFocus={true}
               swipeable={true}
               dynamicHeight={true}
-              animationHandler='fade'
-            >{park.images.map(image => <img src={image} alt={park.title}></img>)}
-          </Carousel>
-        </Container>
-        <Divider hidden/>
-        <Container>
-          <Header as='h3' color='olive'><b>Description</b></Header>
-          <Container>{park.description}</Container>
-          <Weather park={park}/>
+              animationHandler="fade"
+            >
+              {park.images.map((image) => (
+                <img src={image} alt={park.title}></img>
+              ))}
+            </Carousel>
+          </Container>
+          <Divider hidden />
+          <Container>
+            <Header as="h3" color="olive">
+              <b>Description</b>
+            </Header>
+            <Container>{park.description}</Container>
+            <Weather park={park} />
             <Grid columns={3}>
               <Grid.Column>
                 <ParkInfo park={park} />
@@ -69,27 +85,31 @@ const ParkPage = () => {
                 <Activities park={park} />
               </Grid.Column>
             </Grid>
-            <Segment raised color='olive'>
-              <ReactMapGl
-                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-                height='30em'
-                width='100%'
-                mapStyle='mapbox://styles/mapbox/streets-v11'
-                zoom={13}
-                latitude={lat}
+            <Segment raised color="olive" id="map">
+              <Map
+                mapLib={import("mapbox-gl")}
+                mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+                initialViewState={{
+                  longitude: lat,
+                  latitude: long,
+                  zoom: 13,
+                }}
                 longitude={long}
+                latitude={lat}
+                style={{ width: 1100, height: 500 }}
               />
             </Segment>
             <Segment basic />
             <Container>
-              <UserComment id={id}/>
+              <UserComment id={id} />
             </Container>
-          <Divider />
-        </Container>
-      </>
-      }
+            <Divider />
+          </Container>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ParkPage
+export default ParkPage;
